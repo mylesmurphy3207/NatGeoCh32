3e582f77a7a6ffc66ffa57b3c3e7bf27fbb4fba5
mylesmurphy3207 <112994560+mylesmurphy3207@users.noreply.github.com>
2022-09-27 17:20:03 -0700

    java 9/27/22

---
+++ JAVA1/jsScript.js
@@ -0,0 +1,20 @@
+//console.log("here i am again");
+//var mfirstvar = "Myles";
+//myfrstVar = "Naruto";
+//var age= 25;
+
+const tax = 11;
+let userName="Saul";
+let age = 36;
+
+
+
+//console.log("My name is:"+ myfirtVar + " and my age is: "+age);
+//console.log("the constant value is:"+ tax);
+
+document.write("<p> Name :"+userName+" and the age is"+age+ "</p>" );
+
+document.write(`
+<p>Name: ${userName}</p>
+<p>age is: ${age}</p>
+`)
